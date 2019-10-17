a1d719237db080aebf59dc4e874dacdae85f155e
optimum <optimum@192.168.1.40>
2019-10-17 14:27:49 +0530

    css changes

---
+++ TrackEmployee/src/main/java/com/demo/sample/modal/Employee.java
@@ -0,0 +1,72 @@
+package com.demo.sample.modal;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class Employee {
+	@Id
+	private int Id;
+	private String name;
+	private String doj;
+	private int numberOfInterviewsScheduled;
+	private String upcomingInterviews;
+	
+	public String getDoj() {
+		return doj;
+	}
+
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+
+	public int getNumberOfInterviewsScheduled() {
+		return numberOfInterviewsScheduled;
+	}
+
+	public void setNumberOfInterviewsScheduled(int numberOfInterviewsScheduled) {
+		this.numberOfInterviewsScheduled = numberOfInterviewsScheduled;
+	}
+
+	public String getUpcomingInterviews() {
+		return upcomingInterviews;
+	}
+
+	public void setUpcomingInterviews(String upcomingInterviews) {
+		this.upcomingInterviews = upcomingInterviews;
+	}
+
+	public String getClientsAttended() {
+		return clientsAttended;
+	}
+
+	public void setClientsAttended(String clientsAttended) {
+		this.clientsAttended = clientsAttended;
+	}
+
+	private String clientsAttended;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [Id=" + Id + ", name=" + name + ", doj=" + doj + ", numberOfInterviewsScheduled="
+				+ numberOfInterviewsScheduled + ", upcomingInterviews=" + upcomingInterviews + ", clientsAttended="
+				+ clientsAttended + "]";
+	}
+
+}
--- TrackEmployee/src/main/webapp/insert.jsp
+++ TrackEmployee/src/main/webapp/insert.jsp
@@ -1,12 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Add Employee here</title>
+<link rel="stylesheet" type="text/css" href="home.css">
 </head>
+<h1>Add Employee Details</h1>
 <body>
-
+	<label id="idLabel">ID :</label>
+	<input id="id" type="number">
+	<label id="nameLabel">Name :</label>
+	<input id="name" type="text">
+	<label id="dojLabel">Date of Joining :</label>
+	<input id="doj" type="text">
+	<label id="noiLabel">Number of Interviews Scheduled :</label>
+	<input id="noi" type="number">
+	<label id="uiLabel">Upcoming Interviews scheduled :</label>
+	<input id="ui" type="text">
+	<label id="caLabel">Clients Attended :</label>
+	<input id="ca" type="text">
 </body>
 </html>
